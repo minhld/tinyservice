259dcb6e1dd69f6c38826a48cd73f36afb04e2e2
Le Dinh Minh <ledinhminh3883@gmail.com>
2017-10-06 11:41:37 -0600

    adding print JSON responder

--- com/usu/tinyservice/classes/MobileServiceDemo_Log.java
+++
@@ -1,9 +0,0 @@
-package com.usu.tinyservice.classes;
-
-import android.util.Log;
-
-public final class MobileServiceDemo_Log {
-  public static void log(MobileServiceDemo args) {
-    Log.d("MobileServiceDemo", String.format(""));
-  }
-}
