21bbb5c03bf2e80f79fc805e04f07a5216f1b3c9
Le Dinh Minh <ledinhminh3883@gmail.com>
2018-01-12 09:59:52 -0700

    added new test for service B

---
+++ src/com/usu/tinyservice/network/tests/test_remote_serviceB_remote_broker.java
@@ -0,0 +1,28 @@
+package com.usu.tinyservice.network.tests;
+
+import com.usu.tinyservice.network.Broker;
+
+/**
+ * REMOTE SERVICE TEST SUITE - REMOTE BROKER 
+ * (WITH WORKERS PROVIDING REMOTE SERVICES)
+ * ------
+ * - this test locates on a different server (129.123.7.41)
+ * - this provides remote services to the others
+ * - acclaimer: this should be started firstly 
+ * 
+ * @author minhld
+ *
+ */
+public class test_remote_serviceB_remote_broker extends Thread {
+	public void run() {
+		String brokerIp = "129.123.7.172";
+		
+		new Broker(brokerIp);
+
+		new ServiceBWorker(brokerIp);
+	}
+	
+	public static void main(String[] args) {
+		new test_remote_serviceB_remote_broker().start();
+	}
+}
